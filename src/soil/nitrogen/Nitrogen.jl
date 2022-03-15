3cfd6f5c0e48b79a07b8c9dca195e202ca104b6d
Zheng Shi <zhengshi@Zhengs-iMac.local>
2022-03-15 10:44:35 -0500

    N in soil

---
+++ src/soil/nitrogen/Nitrogen.jl
@@ -0,0 +1,11 @@
+## main program
+"""
+note: there are in total 5 soil layers;
+however, I add the 6th layer as a default for whole soil porfile
+for comparison.
+"""
+module main
+
+    
+
+end # module
