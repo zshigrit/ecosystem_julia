349c0d4b75f9342720c3fc984395490da299bea0
zheng shi <zshi.grit@gmail.com>
2022-03-21 00:02:47 -0500

    diverse metabolite

--- src/soil/carbon/functions/ModelRun.jl
+++ src/soil/carbon/functions/ModelRun.jl
@@ -2,49 +2,49 @@
 
 function ModRunDepth!(par::SoilPar,cpools::CPools,fluxes::CFluxes,ilayer::Integer,
     output_ly::DataFrame,input_c::DataFrame,::Type{depth_output})
-# function ModRunDepth!(par::SoilPar,cpools::CPools,ilayer::Integer)
+# output for layers 
 
-for icycle = 1:ncycle
-for iday = 1:365*nyear
-for ihour = 1:24
-input_c_time = input_c[iday,:]
-CPools!(par,cpools,fluxes,input_c_time)  
-end
-end 
-end
-output_ly[ilayer,"Layer"] = ilayer
-output_ly[ilayer,"POMh"] = cpools.POMh
-output_ly[ilayer,"POMo"] = cpools.POMo
-output_ly[ilayer,"MOM"] = cpools.MOM
-output_ly[ilayer,"DOM"] = cpools.DOM
-output_ly[ilayer,"QOM"] = cpools.QOM
-output_ly[ilayer,"MBA"] = cpools.MBA
-output_ly[ilayer,"MBD"] = cpools.MBD
+    for icycle = 1:ncycle
+        for iday = 1:365*nyear
+            for ihour = 1:24
+                input_c_time = input_c[iday,:]
+                CPools!(par,cpools,fluxes,input_c_time)  
+            end
+        end 
+    end
+    output_ly[ilayer,"Layer"] = ilayer
+    output_ly[ilayer,"POMh"] = cpools.POMh
+    output_ly[ilayer,"POMo"] = cpools.POMo
+    output_ly[ilayer,"MOM"] = cpools.MOM
+    output_ly[ilayer,"DOM"] = cpools.DOM
+    output_ly[ilayer,"QOM"] = cpools.QOM
+    output_ly[ilayer,"MBA"] = cpools.MBA
+    output_ly[ilayer,"MBD"] = cpools.MBD
 end 
 
 function ModRunDepth!(par::SoilPar,cpools::CPools,fluxes::CFluxes,ilayer::Integer,output_ly::DataFrame,
     input_c::DataFrame,::Type{daily_output})
 # function ModRunDepth!(par::SoilPar,cpools::CPools,ilayer::Integer)
 
-for icycle = 1:ncycle
-for iday = 1:365*nyear
-for ihour = 1:24
-# global litter_pomo = litter_pomo_array[iday]
-# global litter_pomh = litter_pomh_array[iday]
-# global litter_dom = litter_dom_array[iday]
-input_c_time = input_c[iday,:]
-CPools!(par,cpools,fluxes,input_c_time)
-end
-output_ly[(icycle-1)*365*nyear+iday,"Time"] = (icycle-1)*365*nyear+iday
-output_ly[(icycle-1)*365*nyear+iday,"POMh"] = cpools.POMh
-output_ly[(icycle-1)*365*nyear+iday,"POMo"] = cpools.POMo
-output_ly[(icycle-1)*365*nyear+iday,"MOM"] = cpools.MOM
-output_ly[(icycle-1)*365*nyear+iday,"DOM"] = cpools.DOM
-output_ly[(icycle-1)*365*nyear+iday,"QOM"] = cpools.QOM
-output_ly[(icycle-1)*365*nyear+iday,"MBA"] = cpools.MBA
-output_ly[(icycle-1)*365*nyear+iday,"MBD"] = cpools.MBD
-end 
-end
+    for icycle = 1:ncycle
+        for iday = 1:365*nyear
+            for ihour = 1:24
+                # global litter_pomo = litter_pomo_array[iday]
+                # global litter_pomh = litter_pomh_array[iday]
+                # global litter_dom = litter_dom_array[iday]
+                input_c_time = input_c[iday,:]
+                CPools!(par,cpools,fluxes,input_c_time)
+            end
+            output_ly[(icycle-1)*365*nyear+iday,"Time"] = (icycle-1)*365*nyear+iday
+            output_ly[(icycle-1)*365*nyear+iday,"POMh"] = cpools.POMh
+            output_ly[(icycle-1)*365*nyear+iday,"POMo"] = cpools.POMo
+            output_ly[(icycle-1)*365*nyear+iday,"MOM"] = cpools.MOM
+            output_ly[(icycle-1)*365*nyear+iday,"DOM"] = cpools.DOM
+            output_ly[(icycle-1)*365*nyear+iday,"QOM"] = cpools.QOM
+            output_ly[(icycle-1)*365*nyear+iday,"MBA"] = cpools.MBA
+            output_ly[(icycle-1)*365*nyear+iday,"MBD"] = cpools.MBD
+        end 
+    end
 
 end 
 
